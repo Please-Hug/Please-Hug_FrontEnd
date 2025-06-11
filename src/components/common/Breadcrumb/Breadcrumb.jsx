8d4f846d95bdca02d1b2419a73071166404fd683
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-06-12 00:47:24 +0900

    feat: refactor Breadcrumb component to use dynamic breadcrumb items and improve styling

--- src/components/common/Breadcrumb/Breadcrumb.jsx
+++ src/components/common/Breadcrumb/Breadcrumb.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import styles from "./Breadcrumb.module.scss";
 
+function getBreadcrumbItems() {
+  // This function can be extended to dynamically generate breadcrumb items
+  return [
+    { label: "정휘상(백엔드 3회차)의 대시보드", path: "/" },
+    { label: "홈", path: "/home" },
+  ];
+}
+
 function Breadcrumb() {
+  const breadcrumbItems = getBreadcrumbItems();
+
   return (
     <ul className={styles.breadcrumb}>
-      <li>정휘상(백엔드 3회차)의 대시보드</li>
-      <li>/</li>
-      <li>홈</li>
+      {breadcrumbItems.map((item, index) => (
+        <React.Fragment key={index}>
+          <li className={styles.breadcrumbItem}>
+            <a href={item.path}>{item.label}</a>
+          </li>
+        </React.Fragment>
+      ))}
     </ul>
   );
 }
