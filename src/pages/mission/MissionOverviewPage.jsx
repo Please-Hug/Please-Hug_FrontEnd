afc9bc51f8dc68818dd9c29aacba8b22e3b66cf2
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-06-24 15:15:26 +0900

    feat: 사이드바에 미션 링크 추가 및 미션 개요 페이지 구현

---
+++ src/pages/mission/MissionOverviewPage.jsx
@@ -0,0 +1,318 @@
+import React from "react";
+import styles from "./MissionOverviewPage.module.scss";
+
+function MissionOverviewPage() {
+  return (
+    <div>
+      <h2>미션</h2>
+      <ul className={styles.missionTab}>
+        {/* MissionTab */}
+        <li className={styles.active}>정휘상(백엔드 3회차)의 개인 그룹</li>
+        <li>정휘상(백엔드 3회차)의 개인 알고리즘</li>
+        <li>&nbsp;</li>
+      </ul>
+      <div className={styles.missionTabPage}>
+        <ul>
+          <li>
+            <span>Lv.1</span>
+          </li>
+          <li>
+            <span>Lv.2</span>
+          </li>
+          <li>
+            <span>Lv.4</span>
+          </li>
+          <li>
+            <span>Lv.6</span>
+          </li>
+          <li>
+            <span>Lv.8</span>
+          </li>
+          <li>
+            <span>Lv.12</span>
+          </li>
+          <li>
+            <span>Lv.14</span>
+          </li>
+          <li>
+            <span>Lv.16</span>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+          <li>
+            <p>오리엔테이션</p>
+            <div>
+              <progress value="2" max="14" />
+              <span>2 / 14</span>
+            </div>
+            <div>
+              <span>쉬움</span>
+              <span>(3회차)백엔드 과정 개인 미션</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default MissionOverviewPage;
