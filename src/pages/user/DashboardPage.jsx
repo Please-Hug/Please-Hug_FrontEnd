c54190f4df099e1230096fb04a2be47d6d184be4
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-06-24 15:49:19 +0900

    fix: 대시보드 컴포넌트 경로 대소문자 수정

--- src/pages/user/DashboardPage.jsx
+++ src/pages/user/DashboardPage.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./DashboardPage.module.scss";
 import emptyUserProfile from "../../assets/images/user/empty-user-profile.svg";
-import DashboardMenu from "../../components/dashboard/DashboardMenu";
-import RecentLearning from "../../components/dashboard/RecentLearning";
-import LearningPlans from "../../components/dashboard/LearningPlans";
-import DailyQuest from "../../components/dashboard/DailyQuest";
-import AttendanceCheck from "../../components/dashboard/AttendanceCheck";
-import UserProfile from "../../components/dashboard/UserProfile";
-import RecentDiary from "../../components/dashboard/RecentDiary";
+import DashboardMenu from "../../components/Dashboard/DashboardMenu";
+import RecentLearning from "../../components/Dashboard/RecentLearning";
+import LearningPlans from "../../components/Dashboard/LearningPlans";
+import DailyQuest from "../../components/Dashboard/DailyQuest";
+import AttendanceCheck from "../../components/Dashboard/AttendanceCheck";
+import UserProfile from "../../components/Dashboard/UserProfile";
+import RecentDiary from "../../components/Dashboard/RecentDiary";
 
 function DashboardPage() {
   const userInfo = {
