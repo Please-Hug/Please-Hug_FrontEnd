0d8187fad619c1a910b5812ca454295d755ec99e
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-07-08 09:32:21 +0900

    feat: 미션 추가 모달 컴포넌트 구현

---
+++ src/components/Mission/EditMissionModal.jsx
@@ -0,0 +1,143 @@
+import React, { useState, useEffect, useMemo } from "react";
+import modalStyles from "../../components/common/Modal/Modal.module.scss";
+import Modal from "../../components/common/Modal/Modal";
+import { addMission } from "../../api/missionService";
+
+function EditMissionModal({ isOpen, onClose, missionId }) {
+  const defaultMission = useMemo(() => {
+    return {
+      missionGroupId: 0,
+      name: "",
+      description: "",
+      difficulty: "EASY",
+      rewardPoint: 0,
+      rewardExp: 0,
+      order: 0,
+      line: 0,
+    };
+  }, []);
+
+  const [newMission, setNewMission] = useState(defaultMission);
+
+  const handleAddMission = () => {
+    addMission(newMission)
+      .then(() => {
+        alert("미션이 추가되었습니다.");
+        onClose();
+        setNewMission({
+          ...defaultMission,
+        });
+      })
+      .catch((error) => {
+        console.error("미션 추가 실패:", error);
+      });
+  };
+
+  useEffect(() => {
+    setNewMission({
+      ...defaultMission,
+    });
+  }, [defaultMission]);
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={() => {
+        onClose();
+      }}
+    >
+      <form>
+        <h2>미션 추가</h2>
+        <label>이름</label>
+        <input
+          type="text"
+          value={newMission?.name}
+          onChange={(e) =>
+            setNewMission({
+              ...newMission,
+              name: e.target.value,
+            })
+          }
+        />
+        <label>설명</label>
+        <textarea
+          value={newMission?.description}
+          onChange={(e) =>
+            setNewMission({
+              ...newMission,
+              description: e.target.value,
+            })
+          }
+        ></textarea>
+        <label>난이도</label>
+        <select
+          value={newMission?.difficulty}
+          onChange={(e) =>
+            setNewMission({
+              ...newMission,
+              difficulty: e.target.value,
+            })
+          }
+        >
+          <option value="EASY">EASY</option>
+          <option value="NORMAL">NORMAL</option>
+          <option value="HARD">HARD</option>
+        </select>
+        <label>보상 포인트</label>
+        <input
+          type="number"
+          value={newMission?.rewardPoint}
+          onChange={(e) =>
+            setNewMission({
+              ...newMission,
+              rewardPoint: parseInt(e.target.value),
+            })
+          }
+        />
+        <label>보상 경험치</label>
+        <input
+          type="number"
+          value={newMission?.rewardExp}
+          onChange={(e) =>
+            setNewMission({
+              ...newMission,
+              rewardExp: parseInt(e.target.value),
+            })
+          }
+        />
+        <label>레벨</label>
+        <input
+          type="number"
+          value={newMission?.order}
+          onChange={(e) =>
+            setNewMission({
+              ...newMission,
+              order: parseInt(e.target.value),
+            })
+          }
+        />
+        <label>라인</label>
+        <input
+          type="number"
+          value={newMission?.line}
+          onChange={(e) =>
+            setNewMission({
+              ...newMission,
+              line: parseInt(e.target.value),
+            })
+          }
+        />
+
+        <button
+          type="button"
+          className={modalStyles.saveButton}
+          onClick={handleAddMission}
+        >
+          저장
+        </button>
+      </form>
+    </Modal>
+  );
+}
+
+export default EditMissionModal;
