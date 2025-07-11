d86b768d300f5a1de2471bd1b6ed67c2006de9f3
jam9582 <jjam9582@naver.com>
2025-07-11 21:55:48 +0900

    css 파일만 설정하면 성공

--- src/api/adminService.js
+++ src/api/adminService.js
@@ -1,13 +1,8 @@
-import axios from 'axios';
+import api from './axiosInstance';
 const BASE = '/api/v1/admin/users';
 
-export const getUsers = () =>
-  axios.get(BASE);                 
-export const getUser  = username =>
-  axios.get(`${BASE}/${username}`);
-export const updateUser     = (username, data) =>
-  axios.patch(`${BASE}/${username}`, data);
-export const deleteUser     = username =>
-  axios.delete(`${BASE}/${username}`);
-export const changeUserRole = (username, role) =>
-  axios.patch(`${BASE}/${username}/role`, { role });
+export const getUsers = () => api.get(BASE);
+export const getUser = username => api.get(`${BASE}/${username}`);
+export const updateUser = (username, data) => api.patch(`${BASE}/${username}`, data);
+export const deleteUser = username => api.delete(`${BASE}/${username}`);
+export const changeUserRole = (username, role) => api.patch(`${BASE}/${username}/role`, { role });
